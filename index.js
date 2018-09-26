caeae2b4e9682ea13e5eeb5fa8fdda1df6348b92
inwisloklem <89099109104@mail.ru>
2018-09-26 23:04:19 +0300

    Make decorateLogger dependencies explicit by adding loggerProviders option

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const appendFile = require('./append-file')
 const composeFactory = require('./compose-factory')
 
 const addLogStdout = (target = {}) => {
-  const date = new Date().toLocaleString()
+  const date = new Date().toLocaleString('ru')
 
   target.logStdout = (message, level = 'info') => {
     switch (level) {
       case 'info':
-        console.info(`INFO. ${date}. ${message}`)
+        console.info(`${date}. INFO. ${message}`)
         break
       case 'error':
-        console.error(`ERROR. ${date}. ${message}`)
+        console.error(`${date}. ERROR. ${message}`)
         break
       default:
         throw new Error('No valid log level provided')
@@ -21,15 +21,15 @@ const addLogStdout = (target = {}) => {
 }
 
 const addLogFile = (target = {}, { file = './.log' } = {}) => {
-  const date = new Date().toLocaleString()
+  const date = new Date().toLocaleString('ru')
 
   target.logFile = (message, level = 'info') => {
     switch (level) {
       case 'info':
-        appendFile(file, `INFO. ${date}. ${message}\n`)
+        appendFile(file, `${date}. INFO. ${message}\n`)
         break
       case 'error':
-        appendFile(file, `ERROR. ${date}. ${message}\n`)
+        appendFile(file, `${date}. ERROR. ${message}\n`)
         break
       default:
         throw new Error('No valid log level provided')
@@ -39,15 +39,25 @@ const addLogFile = (target = {}, { file = './.log' } = {}) => {
   return target
 }
 
-const decorateLogger = (target = {}) => {
+const decorateLogger = (target = {}, { loggerProviders = ['logStdout'] }) => {
   target.decorate = function (func, { message, level }) {
     if (typeof func !== 'function') {
       throw new Error('No function provided')
     }
 
     return function () {
-      Object.keys(target)
-        .forEach(k => k.includes('log') ? target[k](message, level) : null)
+      let countLoggers = 0
+
+      loggerProviders.forEach(providerName => {
+        if (target[providerName]) {
+          countLoggers += 1
+          target[providerName](message, level)
+        }
+      })
+
+      if (countLoggers < 1) {
+        throw new Error('Minimum one logger provider required')
+      }
 
       return func()
     }
@@ -57,7 +67,7 @@ const decorateLogger = (target = {}) => {
 }
 
 const createLogger = composeFactory(addLogStdout, addLogFile, decorateLogger)
-const logger = createLogger({ file: './.log2' })
+const logger = createLogger({ file: './.log2', loggerProviders: ['logStdout', 'logFile'] })
 
 function test () {}
 const loggingTest = logger.decorate(test, { message: 'Talk is cheap. Show me the code.', level: 'error' })
